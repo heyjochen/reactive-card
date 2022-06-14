d32410520cf56f03ad1502f4e8fe154b52a34424
heyjochen <jochen@stierberger.com>
2022-06-14 11:42:47 -0500

    add items to data file

--- card/src/data.js
+++ card/src/data.js
@@ -1,12 +1,35 @@
 export default [
   {
-    title: "Mount Fuji",
-    location: "Japan",
+    id: 1,
+    title: "Isla Mujeres",
+    location: "México",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    startDate: "28 Jan, 2022",
+    endDate: "8 Feb, 2022",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "Isla Mujeres is a small island just off the coast of Mexico’s Yucatan Peninsula, directly across the water from Cancun with more than enough activities to keep people of all ages entertained for several days",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    id: 2,
+    title: "Berlin",
+    location: "Germany",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "28 Okt, 2021",
+    endDate: "26 Nov, 2021",
+    description:
+      "Berlin's history of battling ideologies makes for some of the most fascinating sightseeing in Europe. Explore the remnants of the Berlin Wall, the glorious dome atop the Reichstag (Parliament Building) or the peaceful greenery in Tiergarten.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    id: 3,
+    title: "Los Ayala",
+    location: "México",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "14 Aug, 2021",
+    endDate: "24 Aug, 2021",
+    description:
+      "Los Ayala is situated at the foot of the Sierra de Vallejo Mountains. An undiscovered gem, Los Ayala is an authentic Mexican beach town and a fishing village. It is just now beginning to be discovered as a tropical beach destination by foreign tourists.",
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
   },
 ];
