426b3f697d0b561de51dd61fb190ed83bd834020
heyjochen <jochen@stierberger.com>
2022-06-14 11:42:30 -0500

    import cards

--- card/src/App.js
+++ card/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import data from "./data";
+import Card from "./components/Card";
 import Header from "./components/Header";
 
 export default function App() {
-  return <Header />;
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
+  return (
+    <>
+      <Header />
+      <section className="main">{cards}</section>
+    </>
+  );
 }
