74008ddca6b8e1a85950aba4e8144b8b71cbe179
heyjochen <jochen@stierberger.com>
2022-06-14 11:43:35 -0500

    add Header structure

--- card/src/components/Header.js
+++ card/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+import logo from "../assets/sun-logo.svg";
+console.log(logo);
 
-export default function Header() {
+export default function Header(props) {
   return (
     <header className="header">
-      <img src="#" className="header__img" />
-      <a href="#">been there - done that</a>
+      <img src={logo} className="header__img" />
+      <a href="">been there - done that</a>
     </header>
   );
 }
