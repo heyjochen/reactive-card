31d7165c4b3f51064370b1e257532f76876f6540
heyjochen <jochen@stierberger.com>
2022-06-14 10:18:40 -0500

    add Header component

---
+++ card/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="#" className="header__img" />
+      <a href="#">been there - done that</a>
+    </header>
+  );
+}
