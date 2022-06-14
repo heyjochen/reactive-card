52b8948ecaa1f91b34bff846b3a9621d117e2e6a
heyjochen <jochen@stierberger.com>
2022-06-14 11:43:22 -0500

    add structure in Card

--- card/src/components/Card.js
+++ card/src/components/Card.js
@@ -1,9 +1,23 @@
 import React from "react";
-import data from "../data";
 
-// export default function Card() {
-
-//   return (
-
-//   )
-// }
+export default function Card(props) {
+  console.log(props);
+  return (
+    <article className="card">
+      <img src={props.imageUrl} alt="" className="card__img" />
+      <div class="card__text">
+        <div className="card__location">
+          <span className="card__country">{props.location}</span>
+          <a href={props.googleMapsUrl} className="card__maps-link">
+            View on Google Maps
+          </a>
+        </div>
+        <h1 className="card__title">{props.title}</h1>
+        <p className="card__dates">
+          {props.startDate} - {props.endDate}
+        </p>
+        <p className="card__description">{props.description}</p>
+      </div>
+    </article>
+  );
+}
