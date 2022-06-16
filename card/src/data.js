093608b1ad2988d78de770dcf0e6934084a55d16
heyjochen <jochen@stierberger.com>
2022-06-16 15:24:53 -0500

    add image urls

--- card/src/data.js
+++ card/src/data.js
@@ -8,18 +8,20 @@ export default [
     endDate: "8 Feb, 2022",
     description:
       "Isla Mujeres is a small island just off the coast of Mexico’s Yucatan Peninsula, directly across the water from Cancun with more than enough activities to keep people of all ages entertained for several days",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    imageUrl:
+      "https://github.com/heyjochen/reactive-card/blob/main/card/src/assets/IMG_2633.JPG?raw=true",
   },
   {
     id: 2,
-    title: "Berlin",
+    title: "Hamburg",
     location: "Germany",
-    googleMapsUrl: "https://goo.gl/maps/Qj9gNiFes3BmVJGUA",
+    googleMapsUrl: "https://goo.gl/maps/fsNAqpiMpqsHxH8x7",
     startDate: "28 Okt, 2021",
     endDate: "26 Nov, 2021",
     description:
       "Berlin's history of battling ideologies makes for some of the most fascinating sightseeing in Europe. Explore the remnants of the Berlin Wall, the glorious dome atop the Reichstag (Parliament Building) or the peaceful greenery in Tiergarten.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    imageUrl:
+      "https://github.com/heyjochen/reactive-card/blob/main/card/src/assets/IMG_0895.JPG?raw=true",
   },
   {
     id: 3,
@@ -30,6 +32,7 @@ export default [
     endDate: "24 Aug, 2021",
     description:
       "Los Ayala is situated at the foot of the Sierra de Vallejo Mountains. An undiscovered gem, Los Ayala is an authentic Mexican beach town and a fishing village. It is just now beginning to be discovered as a tropical beach destination by foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    imageUrl:
+      "https://github.com/heyjochen/reactive-card/blob/main/card/src/assets/IMG_0395.JPG?raw=true",
   },
 ];
